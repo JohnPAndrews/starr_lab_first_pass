c7f92117290a309fd5c0553b01bd722541fda6dd
Roee Gilron <roee@RoeeG.local>
2017-04-18 10:40:52 -0700

    changes to ml

--- MAIN_plotStimOnStimOff_Beta_PerSubject.m
+++ MAIN_plotStimOnStimOff_Beta_PerSubject.m
@@ -10,45 +10,227 @@ function MAIN_plotStimOnStimOff_Beta_PerSubject()
 
 %% user settings 
 rootdir           = '/Users/roee/Starr_Lab_Folder/Data_Analysis/First_Pass_Data_Analysis/figures/for_nicki/';
-patdir            = 'brpd_07';
+patdir            = 'brpd_09';
 diruse            = fullfile(rootdir, patdir);
 consuse           = {'rest','ipad','walking'};
 colruse           = [1 0 0 0.5;...
                      0 1 0 0.5;...
                      0 0 1 0.5];
 stimcon           = {'on','off'};
+areasuse          = {'stn','m1'}; 
 params.msec2trim  = 1e4;
 params.sr         = 800;
 params.lowcutoff  = 5;
 params.plottype   = 'pwelch';
 params.noisefloor = 80;
+params.LineWidth  = 1; 
+params.Alpha      = 0.1;
 
-%% plot stuff 
-hfig = figure('Visible','off',...
-              'Position',[110  237  1331 496]);
-for s = 1:2
-    for c = 1:length(consuse)
-        ff = findFilesBVQX(diruse,sprintf('*stim-%s*%s*.mat',...
+hfig = figure('Visible','on',...
+              'Position',[657          34        1331        1281]);
+%% plot avg rest in stim on / stim off across stn and m1 
+for a = 1:2 % loop on areas used
+    for s = 1:2 % loop on stim on / off
+        ff = findFilesBVQX(diruse,sprintf('*%s*stim-%s*%s*.mat',...
+            areasuse{a},...
             stimcon{s},...
-            consuse{c}));
+            consuse{1}));
         for f = 1:length(ff)
             load(ff{f});
             data_tr = preproc_trim_data(data,params.msec2trim,params.sr);
             data_tr_prp = preproc_dc_offset_high_pass(data_tr,params);
             data_no_out = zscore(deleteoutliers(data_tr_prp,0.05));
-%             [smoothdata,filtwts ]= eegfilt(data_no_out',800,13, 30);
-            subplot(1,2,s);
-            [~, hplot(c)] = plot_data_freq_domain(data_no_out,params,[]);
-            title(sprintf('Stim %s',stimcon{s}));
-            hplot(c).LineWidth = 2;
-            hplot(c).Color = colruse(c,:);
-            legendttls{c} = consuse{c};
-            hold on;
+            [smoothdata,filtwts ]= eegfilt(data_no_out',800,13, 30);
+            [psdout(f,:),freq] = pwelch(data_tr_prp,length(data_tr_prp),1024,10:1:400,params.sr,'psd');
+            psdoutlog(f,:) = log10(psdout(f,:));  
         end
+        subplot(3,2,a);
+        hold on; 
+        hplot = plot(freq,psdoutlog);
+        for h = 1:size(hplot,1)
+            if s == 1 % stim on 
+                hplot(h).Color = [0 1 0 params.Alpha];
+            else
+                hplot(h).Color = [1 0 0 params.Alpha];
+            end
+            hplot(h).LineWidth = params.LineWidth;
+        end
+        hplot = plot(freq,mean(psdoutlog,1));
+        hplot.LineWidth = 2; 
+        hleg(s) = hplot; 
+        if s == 1 % stim on 
+            hplot.Color     = [0 1 0];
+        else
+            hplot.Color     = [1 0 0]; 
+        end
+
+        figtitle = sprintf('%s %s rest',areasuse{a}, strrep(patdir,'_',' '));
+        htitle = title(figtitle);
+        xtitle = 'Frequency (Hz)';
+        ytitle = 'Power  (log_1_0\muV^2/Hz)';
+        hxlabel = xlabel(xtitle);
+        hylabel = ylabel(ytitle);
+        xlim([min(freq) max(freq)]);
+        
+        ax = ancestor(hplot, 'axes');
+        hyrule = ax.YAxis;
+        hxrule = ax.XAxis;
+        
+        % format plot - size and fonts
+        formatPlot(htitle,hxlabel,hylabel,hxrule,hyrule,hplot);
+
     end
-    hleg = legend([hplot(1), hplot(2), hplot(3)], ...
-        legendttls,'FontSize',14,'FontWeight','bold');
-end 
+    hleg = legend([hleg(1), hleg(2)], ...
+        {'stim on','stim off'},'FontSize',14,'FontWeight','bold');
+end
+
+%% plot rest and walking STIM on in STN and M1 
+rowfac = 2; 
+for a = 1:2 % loop on areas used
+    for s = [1 3]% loop on cons use 
+        ff = findFilesBVQX(diruse,sprintf('*%s*stim-%s*%s*.mat',...
+            areasuse{a},...
+            stimcon{1},... % stim on 
+            consuse{s}));
+        for f = 1:length(ff)
+            load(ff{f});
+            data_tr = preproc_trim_data(data,params.msec2trim,params.sr);
+            data_tr_prp = preproc_dc_offset_high_pass(data_tr,params);
+            data_no_out = zscore(deleteoutliers(data_tr_prp,0.05));
+            [smoothdata,filtwts ]= eegfilt(data_no_out',800,13, 30);
+            [psdout(f,:),freq] = pwelch(data_tr_prp,length(data_tr_prp),1024,10:1:400,params.sr,'psd');
+            psdoutlog(f,:) = log10(psdout(f,:));  
+        end
+        subplot(3,2,a  + rowfac);
+        hold on; 
+        hplot = plot(freq,psdoutlog);
+        for h = 1:size(hplot,1)
+            if s == 1 % stim on 
+                hplot(h).Color = [0 1 0 params.Alpha];
+            else
+                hplot(h).Color = [1 0 0 params.Alpha];
+            end
+            hplot(h).LineWidth = params.LineWidth;
+        end
+        hplot = plot(freq,mean(psdoutlog,1));
+        hplot.LineWidth = 2; 
+        hleg(s) = hplot;
+        if s == 1 % rest  
+            hplot.Color     = [0 1 0];
+        elseif s ==3 % walking 
+            hplot.Color     = [1 0 0]; 
+        end
+        figtitle = sprintf('%s %s stim on',areasuse{a}, strrep(patdir,'_',' '));
+        htitle = title(figtitle);
+        xtitle = 'Frequency (Hz)';
+        ytitle = 'Power  (log_1_0\muV^2/Hz)';
+        hxlabel = xlabel(xtitle);
+        hylabel = ylabel(ytitle);
+        xlim([min(freq) max(freq)]);
+        
+        ax = ancestor(hplot, 'axes');
+        hyrule = ax.YAxis;
+        hxrule = ax.XAxis;
+        
+        % format plot - size and fonts
+        formatPlot(htitle,hxlabel,hylabel,hxrule,hyrule,hplot);
+
+    end
+        hleg = legend([hleg(1), hleg(3)], ...
+        {'rest','walking'},'FontSize',14,'FontWeight','bold');
+
+end
+
+
+
+%% plot rest and walking STIM off in STN and M1 
+rowfac = 4; 
+for a = 1:2 % loop on areas used
+    for s = [1 3]% loop on cons use 
+        ff = findFilesBVQX(diruse,sprintf('*%s*stim-%s*%s*.mat',...
+            areasuse{a},...
+            stimcon{2},... % stim off 
+            consuse{s}));
+        for f = 1:length(ff)
+            load(ff{f});
+            data_tr = preproc_trim_data(data,params.msec2trim,params.sr);
+            data_tr_prp = preproc_dc_offset_high_pass(data_tr,params);
+            data_no_out = zscore(deleteoutliers(data_tr_prp,0.05));
+            [smoothdata,filtwts ]= eegfilt(data_no_out',800,13, 30);
+            [psdout(f,:),freq] = pwelch(data_tr_prp,length(data_tr_prp),1024,10:1:400,params.sr,'psd');
+            psdoutlog(f,:) = log10(psdout(f,:));  
+        end
+        subplot(3,2,a  + rowfac);
+        hold on; 
+        hplot = plot(freq,psdoutlog);
+        for h = 1:size(hplot,1)
+            if s == 1 % stim on 
+                hplot(h).Color = [0 1 0 params.Alpha];
+            else
+                hplot(h).Color = [1 0 0 params.Alpha];
+            end
+            hplot(h).LineWidth = params.LineWidth;
+        end
+        hplot = plot(freq,mean(psdoutlog,1));
+        hplot.LineWidth = 2;
+        hleg(s) = hplot;
+        if s == 1 % rest  
+            hplot.Color     = [0 1 0];
+        elseif s ==3 % walking 
+            hplot.Color     = [1 0 0]; 
+        end
+        figtitle = sprintf('%s %s stim off',areasuse{a}, strrep(patdir,'_',' '));
+        htitle = title(figtitle);
+        xtitle = 'Frequency (Hz)';
+        ytitle = 'Power  (log_1_0\muV^2/Hz)';
+        hxlabel = xlabel(xtitle);
+        hylabel = ylabel(ytitle);
+        xlim([min(freq) max(freq)]);
+        
+        ax = ancestor(hplot, 'axes');
+        hyrule = ax.YAxis;
+        hxrule = ax.XAxis;
+        
+        % format plot - size and fonts
+        formatPlot(htitle,hxlabel,hylabel,hxrule,hyrule,hplot);
+
+    end
+    hleg = legend([hleg(1), hleg(3)], ...
+        {'rest','walking'},'FontSize',14,'FontWeight','bold');
+
+end
+hfig.PaperPositionMode = 'auto';
+hfig.PaperOrientation = 'landscape';
+hfig.PaperSize = [20,20]; 
+save_figure(hfig,[patdir 'nicki_plot_no_preproc'],diruse,'pdf')
+
+return; 
+
+%% plot stuff 
+for a = 1:2 % loop on areas used
+    for s = 1:2 % loop on stim on / off
+        for c = 1:length(consuse) % loop on cons used
+            ff = findFilesBVQX(diruse,sprintf('*stim-%s*%s*.mat',...
+                stimcon{s},...
+                consuse{c}));
+            for f = 1:length(ff)
+                load(ff{f});
+                data_tr = preproc_trim_data(data,params.msec2trim,params.sr);
+                data_tr_prp = preproc_dc_offset_high_pass(data_tr,params);
+                data_no_out = zscore(deleteoutliers(data_tr_prp,0.05));
+                %             [smoothdata,filtwts ]= eegfilt(data_no_out',800,13, 30);
+                subplot(1,2,s);
+                [~, hplot(c)] = plot_data_freq_domain(data_no_out,params,[]);
+                title(sprintf('Stim %s',stimcon{s}));
+                hplot(c).LineWidth = 2;
+                hplot(c).Color = colruse(c,:);
+                legendttls{c} = consuse{c};
+                hold on;
+            end
+        end
+        hleg = legend([hplot(1), hplot(2), hplot(3)], ...
+            legendttls,'FontSize',14,'FontWeight','bold');
+    end
 hfig.PaperPositionMode = 'auto';
 hfig.PaperOrientation = 'landscape';
 hfig.PaperSize = [20,20]; 
--- compareStimOnStimOff_machine_learning.m
+++ compareStimOnStimOff_machine_learning.m
@@ -11,14 +11,26 @@ m1figStimOff = fullfile(figdir,'m1_stimOff');mkdir(m1figStimOff)
 
 [settings, params] = get_settings_params();
 load(fullfile(resultsdir,'all_session_celldb.mat'),'outdb','sessiondb','symptomcat');
+%% report pat session count 
+unqpat = unique(sessiondb.patientcode); 
+for p = 1:length(unqpat)
+    patuse = unqpat{p};
+    sescount(p) = ...
+        sum(logical(strcmp(sessiondb.patientcode,patuse)));
+    fprintf('pat %s has %d\n',...
+        patuse,...
+        sescount(p)); 
+end
 %% set params.
 % inclusion criteria
+%% FIX SELETION CRITERIA 
+patuse = 'brpd_09';
 logidxoverall = ...
     sessiondb.usevisit == 1 & ...
     ~strcmp(sessiondb.visitCategory,'000000') & ...
     logical(strcmp(sessiondb.StimOn,'on') |  strcmp(sessiondb.StimOn,'off') ) & ...
     logical(strcmp(sessiondb.Medication,'on'))| logical(strcmp(sessiondb.Medication,'off')) & ...
-    logical(strcmp(sessiondb.patientcode,'brpd_01')) &  ...
+    logical(strcmp(sessiondb.patientcode,patuse)) &  ...
     sessiondb.sr == 800;
 newdb = sessiondb(logidxoverall,:);
 fprintf('%d stim off, %d stim on\n',...
@@ -32,11 +44,11 @@ fprintf('%d meds on, %d meds off\n',...
 labelarea = {'stn','m1'};
 chanidx = [1 3];
 params.noisefloor = 400;
-params.cvfold         = 5; 
+params.cvfold         = 5;
 % hfig = figure('visible','on','position',[170        -330        2264        1056]);
 cntdat = 1;
 for i = 1:size(newdb,1)
-    for cu = 1%:2 % only use stn data 
+    for cu = 1%:2 % only use stn data
         ticOpen = tic;
         start = tic;
         data = importdata(newdb.datafullpath{i});
@@ -44,24 +56,24 @@ for i = 1:size(newdb,1)
         params.sr = newdb.sr(i);
         data_tr = preproc_trim_data(data,params.msec2trim,params.sr);
         data_tr_prp = preproc_dc_offset_high_pass(data_tr,params);
-        clear data; 
-        %% data valiation: 
-        analyzesession = ~isempty(data_tr_prp(~isnan(data_tr_prp))); % check NaN's 
-        % length 
-        if length(data_tr)/params.sr < 20; analyzesession = 0; end; 
-        if sum(data_tr_prp) == 0; analyzesession = 0; end; % empty channel 
-        %% get data 
+        clear data;
+        %% data valiation:
+        analyzesession = ~isempty(data_tr_prp(~isnan(data_tr_prp))); % check NaN's
+        % length
+        if length(data_tr)/params.sr < 20; analyzesession = 0; end;
+        if sum(data_tr_prp) == 0; analyzesession = 0; end; % empty channel
+        %% get data
         if analyzesession
             data_no_out = zscore(deleteoutliers(data_tr_prp,0.05));
-            %% split into training set and testing set  
-            c = cvpartition( length(data_no_out),'Kfold',params.cvfold); 
+            %% split into training set and testing set
+            c = cvpartition( length(data_no_out),'Kfold',params.cvfold);
             for cc = 1:c.NumTestSets
-                cvec(:,cc) = c.test(cc).* cc; 
+                cvec(:,cc) = c.test(cc).* cc;
             end
-            testsets = sort(sum(cvec,2)); 
+            testsets = sort(sum(cvec,2));
             for cc = 1:c.NumTestSets
-                testsetlog(:,cc) = testsets == cc; 
-                trainsetlog(:,cc) = testsets ~= cc; 
+                testsetlog(:,cc) = testsets == cc;
+                trainsetlog(:,cc) = testsets ~= cc;
             end
             
             for cc = 1:c.NumTestSets
@@ -84,15 +96,15 @@ for i = 1:size(newdb,1)
                 rawdata(cc).test  = data_no_out(testsetlog(:,cc));
                 [dataout(cc).train(cntdat,:),freq] = pwelch(rawdata(cc).train,segLength,NFFT,1:2:400,params.sr,'psd');
                 [dataout(cc).test(cntdat,:),freq] = pwelch(rawdata(cc).test,segLength,NFFT,1:2:400,params.sr,'psd');
-                if sum(dataout(cc).train(cntdat,:)) == 0 
-                    x = 2; 
+                if sum(dataout(cc).train(cntdat,:)) == 0
+                    x = 2;
                 end
                 
             end
             clear cvec c testsetlog trainsetlog testsets
-            mlrow(cntdat,:) = newdb(i,:); 
-            cntdat = cntdat + 1; 
-
+            mlrow(cntdat,:) = newdb(i,:);
+            cntdat = cntdat + 1;
+            
             figname = sprintf('%0.3d_%s_p-%s_v-%s_stim-%s_serial-%d',...
                 i,...
                 labelarea{cu},...
@@ -104,25 +116,26 @@ for i = 1:size(newdb,1)
         fprintf('%d out of %d done in %f\t %f to go\n',...
             i,...
             size(newdb,1),...
-            toc(ticOpen),... 
+            toc(ticOpen),...
             toc(ticOpen) * (size(newdb,1) - i));
     end
 end
-save('temp_ml_ds.mat','dataout','mlrow'); 
-
-%% classify stim on / stim off 
-% try to balance the classifier 
+fn = sprintf('ML_dat_p-%s_.mat',patuse); 
+save(fn,'dataout','mlrow');
+return; 
+%% classify stim on / stim off
+% try to balance the classifier
 unqelec = unique(mlrow.stn_electrodes);
 for i = 1:length(unqelec)
     ucnt(i) = sum(strcmp(mlrow.stn_electrodes,unqelec{i}));
 end
 max(ucnt)
-% loop on visits 
-unqvisits = unique(mlrow.visitCategory); 
-for v = 1:length(unqvisits)
-    logvis = strcmp(unqvisits,
-    
-end
+% loop on visits
+% unqvisits = unique(mlrow.visitCategory);
+% for v = 1:length(unqvisits)
+%     logvis = strcmp(unqvisits,
+%
+% end
 
 fprintf('%d stim off, %d stim on\n',...
     sum(logical(strcmp(mlrow.StimOn,'on'))),...
@@ -132,21 +145,132 @@ fprintf('%d meds on, %d meds off\n',...
     sum(logical(strcmp(mlrow.Medication,'on'))),...
     sum(logical(strcmp(mlrow.Medication,'off'))));
 
-% get labels 
+% make a smaller database with rows 
+unqtasks = unique(mlrow.ConditionTask);
+for u = 1:length(unqtasks)
+    fprintf('%d instances of %s condition\n',...
+        sum(logical(strcmp(mlrow.ConditionTask,...
+        unqtasks{u}))),...
+        unqtasks{u});        
+end
+
+conuse = {'ipad','talking','walking'}; 
+for c = 1:length(conuse)
+    logcon(:,c) = strcmp(mlrow.ConditionTask,conuse{c}).*c;
+end
+labels = sum(logcon,2); 
+idxuse = ~labels==0; 
+labels = labels(idxuse); 
+for i = 1:params.cvfold
+    dataout(i).train = dataout(i).train(idxuse,:); 
+    dataout(i).test  = dataout(i).test(idxuse,:); 
+end
+
+%% plot the data
+hfig = figure;
+cntplt = 1;
 for i = 1:params.cvfold
     labels = strcmp(mlrow.StimOn,'on');
+    subplot(5,2,cntplt); cntplt = cntplt + 1;
+    hplot = plot(1:200,log10(dataout(i).train));
+    for h = 1:size(hplot,1)
+        if labels(h)
+            hplot(h).Color = [0 0 1 0.2];
+            hplot(h).LineWidth = 0.5;
+        else
+            hplot(h).Color = [1 0 0 0.2];
+            hplot(h).LineWidth = 0.5;
+        end
+    end
+    legend({'on','off'});
+    title(sprintf('train %d',i));
+    subplot(5,2,cntplt); cntplt = cntplt + 1;
+    hplot = plot(1:200,log10(dataout(i).test));
+    for h = 1:size(hplot,1)
+        if labels(h)
+            hplot(h).Color = [0 0 1 0.2];
+            hplot(h).LineWidth = 0.5;
+        else
+            hplot(h).Color = [1 0 0 0.2];
+            hplot(h).LineWidth = 0.5;
+        end
+    end
+    legend({'on','off'});
+    title(sprintf('test %d',i));
+end
+
+
+%% do some calcs
+% get labels
+hfig = figure;
+cntplt = 1;
+for i = 1:params.cvfold
+    labels = strcmp(mlrow.Medication,'on');
     model = svmtrain(double(labels),dataout(i).train, '-t 0' );
+    subplot(5,2,cntplt); cntplt = cntplt + 1;
+    plot(1:200,log10(dataout(i).train));
+    title(sprintf('train %d',i));
     [predicted_label, accuracy, third] = svmpredict(double(labels), dataout(i).test, model);
-    acc(i) = accuracy(1);  
+    acc(i) = accuracy(1);
+    subplot(5,2,cntplt); cntplt = cntplt + 1;
+    plot(1:200,log10(dataout(i).test));
+    title(sprintf('test %d',i));
 end
 
 labels = strcmp(mlrow.Medication,'on');
 
 for i = 1:params.cvfold
-    labels = strcmp(mlrow.Medication,'on');
+%     labels = strcmp(mlrow.Medication,'on');
     model = svmtrain(double(labels),dataout(i).train, '-t 0' );
     [predicted_label, accuracy, third] = svmpredict(double(labels), dataout(i).test, model);
-    acc(i) = accuracy(1);  
+    acc(i) = accuracy(1);
 end
 
+%% evaluate using matlab's built in ML functions:
+for i = 1:params.cvfold
+    resp = strcmp(mlrow.Medication,'on');
+    pred = log10(dataout(i).train);
+    % Fit a logistic regression model to estimate the posterior probabilty
+    % of class A vs B
+    mdl = fitglm(pred,resp,'Distribution','binomial','Link','logit');
+    score_log = mdl.Fitted.Probability; % Probability estimates
+    %%
+    % Compute the standard ROC curve using the probabilities for scores.
+    [Xlog,Ylog,Tlog,AUClog] = perfcurve(resp,score_log,'true');
+    %%
+    % Train an SVM classifier on the same sample data. Standardize the data.
+    mdlSVM = fitcsvm(pred,resp,'Standardize',true);
+    %%
+    % Compute the posterior probabilities (scores).
+    mdlSVM = fitPosterior(mdlSVM);
+    [~,score_svm] = resubPredict(mdlSVM);
+    %%
+    % The second column of |score_svm| contains the posterior probabilities
+    % of bad radar returns.
+    %%
+    % Compute the standard ROC curve using the scores from the SVM model.
+    [Xsvm,Ysvm,Tsvm,AUCsvm] = perfcurve(resp,score_svm(:,mdlSVM.ClassNames),'true');
+    %%
+    % Fit a naive Bayes classifier on the same sample data.
+    mdlNB = fitcnb(pred,resp);
+    %%
+    % Compute the posterior probabilities (scores).
+    [~,score_nb] = resubPredict(mdlNB);
+    %%
+    % Compute the standard ROC curve using the scores from the naive Bayes
+    % classification.
+    [Xnb,Ynb,Tnb,AUCnb] = perfcurve(resp,score_nb(:,mdlNB.ClassNames),'true');
+    %%
+    % Plot the ROC curves on the same graph.
+    plot(Xlog,Ylog)
+    hold on
+    plot(Xsvm,Ysvm)
+    plot(Xnb,Ynb)
+    legend('Logistic Regression','Support Vector Machines','Naive Bayes','Location','Best')
+    xlabel('False positive rate'); ylabel('True positive rate');
+    title('ROC Curves for Logistic Regression, SVM, and Naive Bayes Classification')
+    hold off
+    AUClog
+    AUCsvm
+    AUCnb
 end
